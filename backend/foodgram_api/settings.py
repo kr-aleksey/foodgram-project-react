0e6e9acfbee7d94f50b567c1118fbdc62eb85dfd
Aleksey Kravtsun <kravtsun.aleksey@gmail.com>
2023-03-10 16:42:37 +0300

    fix settings

--- backend/foodgram_api/settings.py
+++ backend/foodgram_api/settings.py
@@ -10,10 +10,10 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-$z&rmfg0ol6h1&&)m&hih_estd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['http://127.0.0.1', 'http://158.160.53.6']
-CORS_ALLOWED_ORIGINS = ['http://127.0.0.1', 'http://158.160.53.6']
+ALLOWED_HOSTS = ['http://127.0.0.1:8000', 'http://backend']
+CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:8000', 'http://backend']
 # CORS_ALLOW_ALL_ORIGINS = True
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1', 'http://158.160.53.6']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://backend']
 
 # Application definition
 
